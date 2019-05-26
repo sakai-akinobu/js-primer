96c0ddf5dce3ffe9ba06fb83f19c2c36c65d9b6b
azu <azuciao@gmail.com>
2019-05-26 12:50:11 +0000

    Deploy GitBook build [skip ci]

---
+++ sw.js
@@ -0,0 +1,580 @@
+// workbox init setting
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js"
+);
+workbox.core.setCacheNameDetails({ prefix: "js-primer-v1" });
+workbox.googleAnalytics.initialize();
+// reload asap
+workbox.skipWaiting();
+workbox.clientsClaim();
+// precache
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "appendix/links/index.html",
+    "revision": "b501a948175ef61982225f556be48069"
+  },
+  {
+    "url": "basic/array/index.html",
+    "revision": "e1c5d223dcbc8014d91c04b4d2a21726"
+  },
+  {
+    "url": "basic/async/img/promise-chain.png",
+    "revision": "e26f9e8ec157282aeb60810f23a52a6b"
+  },
+  {
+    "url": "basic/async/img/then-rejected-promise.png",
+    "revision": "c45405c987bb1a739850f71be6d010f0"
+  },
+  {
+    "url": "basic/async/index.html",
+    "revision": "edc0507f54faae5b025160d84d1ef6c4"
+  },
+  {
+    "url": "basic/class/index.html",
+    "revision": "8b895f0f67a52eab10485b3086586300"
+  },
+  {
+    "url": "basic/comments/index.html",
+    "revision": "6b80a3084e02fc771acfbcf330ac7a8d"
+  },
+  {
+    "url": "basic/condition/index.html",
+    "revision": "ad8cd42de3f083f7cf08e7b8fdf029f6"
+  },
+  {
+    "url": "basic/data-type/index.html",
+    "revision": "eae01989a482dba188b3f0f98bda0d3e"
+  },
+  {
+    "url": "basic/date/index.html",
+    "revision": "9c68e26ff0036fef4e446fd4cfacc8db"
+  },
+  {
+    "url": "basic/ecmascript/index.html",
+    "revision": "2051d0236be15acca177afe0392a228e"
+  },
+  {
+    "url": "basic/error-try-catch/img/console.error.png",
+    "revision": "aca611880ab3cb034578a248b94f7b44"
+  },
+  {
+    "url": "basic/error-try-catch/img/error.png",
+    "revision": "dff0e4322cb06d42d699119b6f89428f"
+  },
+  {
+    "url": "basic/error-try-catch/index.html",
+    "revision": "6a2b74ffa7be91de7166be3ac140c5c8"
+  },
+  {
+    "url": "basic/error-try-catch/src/console/index.html",
+    "revision": "f1d5ff6a6b82e975f74ac1e3d877f723"
+  },
+  {
+    "url": "basic/error-try-catch/src/error.html",
+    "revision": "359cbe7900932fac79eb321c76e104b4"
+  },
+  {
+    "url": "basic/function-declaration/index.html",
+    "revision": "bd2c0d3e9858390d297ea4db404fab2f"
+  },
+  {
+    "url": "basic/function-scope/index.html",
+    "revision": "eac281ebf6801df88ed2f9d49d7d0ed6"
+  },
+  {
+    "url": "basic/function-this/index.html",
+    "revision": "60a8ef50f0aa4fb5f007024917546427"
+  },
+  {
+    "url": "basic/implicit-coercion/img/JavaScript-Equality-Table.png",
+    "revision": "249e75cfe1f22458bfa9fe71480a6c0d"
+  },
+  {
+    "url": "basic/implicit-coercion/index.html",
+    "revision": "9ed9e212d46a9fa52d4e314cf8d96317"
+  },
+  {
+    "url": "basic/index.html",
+    "revision": "a9f55701291fe442f62034e79215ba95"
+  },
+  {
+    "url": "basic/introduction/img/javascript-ecmascript.png",
+    "revision": "40a83bcf5b26783fc68b7caeb792d36d"
+  },
+  {
+    "url": "basic/introduction/index.html",
+    "revision": "032f632f71c604c1d647eca10d8a8c52"
+  },
+  {
+    "url": "basic/json/index.html",
+    "revision": "7623ca44455f62b05c4915e695130ea4"
+  },
+  {
+    "url": "basic/loop/index.html",
+    "revision": "367f57313bb0a677d2f1159db91e6d10"
+  },
+  {
+    "url": "basic/loop/public/index.html",
+    "revision": "226c5a5e385446f7b048d1b990a8f603"
+  },
+  {
+    "url": "basic/map-and-set/index.html",
+    "revision": "b1e713329d3351b07ae1b800e596f905"
+  },
+  {
+    "url": "basic/math/index.html",
+    "revision": "2bb64541fba48cd6f2b8b793d0203d56"
+  },
+  {
+    "url": "basic/object/index.html",
+    "revision": "ed40e55fdc82f84041880c196eb5800d"
+  },
+  {
+    "url": "basic/operator/index.html",
+    "revision": "e4338006fd2f178f3f551cd385ceff48"
+  },
+  {
+    "url": "basic/other-parts/index.html",
+    "revision": "3b2008a9b332edebb3577ee3a4b75184"
+  },
+  {
+    "url": "basic/prototype-object/img/object-prototype.png",
+    "revision": "6bbe9c151a73ec89ed45606a0b42975f"
+  },
+  {
+    "url": "basic/prototype-object/index.html",
+    "revision": "86a56728681a9ff0c7b9b19e22b66b11"
+  },
+  {
+    "url": "basic/read-eval-print/img/syntax-error.png",
+    "revision": "889dfb7dec547bc8bde6fed9566d204f"
+  },
+  {
+    "url": "basic/read-eval-print/img/web-console.png",
+    "revision": "23eb78c09ad1d984cfa76270d8467b24"
+  },
+  {
+    "url": "basic/read-eval-print/index.html",
+    "revision": "e7d72e74bb80e01530b615350b144d65"
+  },
+  {
+    "url": "basic/read-eval-print/src/empty/index.html",
+    "revision": "5a2a8b11dda21e4b54164d24dd751dc4"
+  },
+  {
+    "url": "basic/read-eval-print/src/example/index.html",
+    "revision": "e6feacbd6b95841d9be147d6d820014f"
+  },
+  {
+    "url": "basic/read-eval-print/src/invalid/syntax-error-typo/index.html",
+    "revision": "e6feacbd6b95841d9be147d6d820014f"
+  },
+  {
+    "url": "basic/read-eval-print/src/invalid/syntax-error/index.html",
+    "revision": "e6feacbd6b95841d9be147d6d820014f"
+  },
+  {
+    "url": "basic/read-eval-print/src/runtime-error/index.html",
+    "revision": "e6feacbd6b95841d9be147d6d820014f"
+  },
+  {
+    "url": "basic/statement-expression/index.html",
+    "revision": "ae71bb2ab86f9bf97f79037d97f57fba"
+  },
+  {
+    "url": "basic/string-unicode/img/codeunit-codepoint-table.png",
+    "revision": "7d065a8e65d944b1898b9de5e8d1e07e"
+  },
+  {
+    "url": "basic/string-unicode/img/emoji-codeunit-codepoint-table.png",
+    "revision": "6665ae16a9f3b2bfee3d28930988f382"
+  },
+  {
+    "url": "basic/string-unicode/img/extenal-code-and-internal-code.png",
+    "revision": "81bdae3abbfa82c8a14ce1d961bab2e5"
+  },
+  {
+    "url": "basic/string-unicode/index.html",
+    "revision": "5928a23cc95ee1d076b24fbdeee1168a"
+  },
+  {
+    "url": "basic/string/index.html",
+    "revision": "3be9f6f3561b167582afc591ae63ee6a"
+  },
+  {
+    "url": "basic/variables/index.html",
+    "revision": "7dd159b8731e1f27458f2312a1165e6e"
+  },
+  {
+    "url": "basic/wrapper-object/index.html",
+    "revision": "dbdaf84da09591051275f49a043ec43d"
+  },
+  {
+    "url": "gitbook/fonts/fontawesome/fontawesome-webfont.eot",
+    "revision": "25a32416abee198dd821b0b17a198a8f"
+  },
+  {
+    "url": "gitbook/fonts/fontawesome/fontawesome-webfont.svg",
+    "revision": "d7c639084f684d66a1bc66855d193ed8"
+  },
+  {
+    "url": "gitbook/fonts/fontawesome/fontawesome-webfont.ttf",
+    "revision": "1dc35d25e61d819a9c357074014867ab"
+  },
+  {
+    "url": "gitbook/fonts/fontawesome/fontawesome-webfont.woff",
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148"
+  },
+  {
+    "url": "gitbook/fonts/fontawesome/fontawesome-webfont.woff2",
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b"
+  },
+  {
+    "url": "gitbook/fonts/fontawesome/FontAwesome.otf",
+    "revision": "5dc41d8fe329a22fa1ee9225571c843e"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-anchors/plugin.css",
+    "revision": "45deda85e7ceaacadb6a51c17248ad1c"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-fontsettings/fontsettings.js",
+    "revision": "fab8f6412ce18bb367635b1bcae503ca"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-fontsettings/website.css",
+    "revision": "056a6db3eef3553a78f3b7e02356b2e7"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-ga/plugin.js",
+    "revision": "8b0d0bfffa07cfd2675fffb1b7b6e6f4"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-github-issue-feedback/plugin.js",
+    "revision": "096a3a5838459d91e482dff2e1230c84"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-highlight/ebook.css",
+    "revision": "fa203ae16ad9f01f4d20061fb9e7a6cc"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-highlight/website.css",
+    "revision": "acce01e3e11cbd4b3882e7732d81f954"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-js-console/console-ui.js",
+    "revision": "0ca768e48d364ee90d22664568902840"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-page-toc-button/plugin.css",
+    "revision": "4e7efddf5df2ea927a186116ba2aee2b"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-page-toc-button/plugin.js",
+    "revision": "834dad580dcb5926f3b090502550f60d"
+  },
+  {
+    "url": "gitbook/gitbook-plugin-sharing/buttons.js",
+    "revision": "e7c1c051d685b9e7530c1a6675e6b119"
+  },
+  {
+    "url": "gitbook/gitbook.js",
+    "revision": "e53bf9037b1d1c9810486ef4c5493624"
+  },
+  {
+    "url": "gitbook/icons/favicon.ico",
+    "revision": "4724b6e29e34167169d6d9e14b8c853f"
+  },
+  {
+    "url": "gitbook/icons/icon-128x128.png",
+    "revision": "b91df37069569bafccc6936d1b2e1352"
+  },
+  {
+    "url": "gitbook/icons/icon-144x144.png",
+    "revision": "e744cdec12fa24a28b40fde9b52d7695"
+  },
+  {
+    "url": "gitbook/icons/icon-152x152.png",
+    "revision": "77dac1368e193743d56106145e1c04a9"
+  },
+  {
+    "url": "gitbook/icons/icon-192x192.png",
+    "revision": "e3682439a02773056ce1bd46e6085555"
+  },
+  {
+    "url": "gitbook/icons/icon-384x384.png",
+    "revision": "2814afaf288c99d08c584b39234d3e39"
+  },
+  {
+    "url": "gitbook/icons/icon-512x512.png",
+    "revision": "fd7a37b059ccac4d7256226883672bce"
+  },
+  {
+    "url": "gitbook/icons/icon-72x72.png",
+    "revision": "406eb5ae0057f1f40b2abf9b17cd12a4"
+  },
+  {
+    "url": "gitbook/icons/icon-96x96.png",
+    "revision": "7529c23361ebe1eaba84a6cd042a9565"
+  },
+  {
+    "url": "gitbook/images/apple-touch-icon-precomposed-152.png",
+    "revision": "77dac1368e193743d56106145e1c04a9"
+  },
+  {
+    "url": "gitbook/images/favicon.ico",
+    "revision": "4724b6e29e34167169d6d9e14b8c853f"
+  },
+  {
+    "url": "gitbook/style.css",
+    "revision": "88a3a50e3559bc577c1be0de4fcc6c6d"
+  },
+  {
+    "url": "gitbook/theme.js",
+    "revision": "176e71ac3bf185b7f08e0f6cb919f1e8"
+  },
+  {
+    "url": "index.html",
+    "revision": "318ed2d350f9db0449386771afe7221b"
+  },
+  {
+    "url": "intro/feedback/index.html",
+    "revision": "d13a3c11cada2ef348e1c79993ec76f2"
+  },
+  {
+    "url": "intro/index.html",
+    "revision": "5361ca1c865b1cba96d54655ee7e5028"
+  },
+  {
+    "url": "intro/preparation/index.html",
+    "revision": "d79ec5234c827692626288cbb56e567e"
+  },
+  {
+    "url": "landing/css/style.css",
+    "revision": "026290c57783f482c4226e004e2279e5"
+  },
+  {
+    "url": "landing/img/cover.png",
+    "revision": "fec746cff0a8b4a9fb4193d4af3d534f"
+  },
+  {
+    "url": "landing/img/js-primer.png",
+    "revision": "19d98be248101b2685bb2a74d510890f"
+  },
+  {
+    "url": "landing/img/repo-actions-watch.png",
+    "revision": "4b4cd63c1bad3861502d3127c2a2d0a2"
+  },
+  {
+    "url": "landing/index.html",
+    "revision": "1bf50123dd7fe8053c579742357ec970"
+  },
+  {
+    "url": "outro/index.html",
+    "revision": "0c4998d1217b11422616877fb0cd557c"
+  },
+  {
+    "url": "use-case/ajaxapp/display/img/fig-1.png",
+    "revision": "15072f06aa7fd4d5fbce148bc2db975f"
+  },
+  {
+    "url": "use-case/ajaxapp/display/index.html",
+    "revision": "63e11c6826c5e7e25e62dd76870bc180"
+  },
+  {
+    "url": "use-case/ajaxapp/display/src/index.html",
+    "revision": "b89416cb1388482fc234c4bf894e0e83"
+  },
+  {
+    "url": "use-case/ajaxapp/entrypoint/img/fig-1.png",
+    "revision": "529c6091a9875e4151bbd301a9eeaeee"
+  },
+  {
+    "url": "use-case/ajaxapp/entrypoint/index.html",
+    "revision": "6601ea1c06b1c6ba530a93566cb07bf2"
+  },
+  {
+    "url": "use-case/ajaxapp/entrypoint/src/index.html",
+    "revision": "59d564baee8b85ccee9e5e5eac419944"
+  },
+  {
+    "url": "use-case/ajaxapp/index.html",
+    "revision": "bc6e9be8520b2d810882a76bb7daad68"
+  },
+  {
+    "url": "use-case/ajaxapp/promise/img/fig-1.png",
+    "revision": "97b1897743ee44e2fd8a9315a7af15eb"
+  },
+  {
+    "url": "use-case/ajaxapp/promise/index.html",
+    "revision": "22d3f28a4dcda3ebbb9f7eb8cb254a2f"
+  },
+  {
+    "url": "use-case/ajaxapp/promise/src/index.html",
+    "revision": "08f26d218b17fbc1aea4461f2a01df19"
+  },
+  {
+    "url": "use-case/ajaxapp/src/index.html",
+    "revision": "08f26d218b17fbc1aea4461f2a01df19"
+  },
+  {
+    "url": "use-case/ajaxapp/xhr/img/fig-1.png",
+    "revision": "30ae672fc36c3cf614eea13ea84dca42"
+  },
+  {
+    "url": "use-case/ajaxapp/xhr/img/fig-2.png",
+    "revision": "37814c55beb033fe5d7d40c0b45afe91"
+  },
+  {
+    "url": "use-case/ajaxapp/xhr/index.html",
+    "revision": "30882960d3747372f55cdaa68b1ef4c0"
+  },
+  {
+    "url": "use-case/ajaxapp/xhr/src/index.html",
+    "revision": "45577067ec45b8853a9b639641dd7dd5"
+  },
+  {
+    "url": "use-case/index.html",
+    "revision": "59c0ccc3aa069e0a8e562772cd4d2880"
+  },
+  {
+    "url": "use-case/module/index.html",
+    "revision": "0bed789a15fd5fba6823198335ac825b"
+  },
+  {
+    "url": "use-case/nodecli/argument-parse/index.html",
+    "revision": "0d5536773769df3c9569622e68066962"
+  },
+  {
+    "url": "use-case/nodecli/helloworld/index.html",
+    "revision": "0bccd5ade7c7eae629f37673ad18e211"
+  },
+  {
+    "url": "use-case/nodecli/index.html",
+    "revision": "4960d41e414b106116eae7ede6487cd0"
+  },
+  {
+    "url": "use-case/nodecli/md-to-html/index.html",
+    "revision": "f6f523ed61972621b1e68f526880029d"
+  },
+  {
+    "url": "use-case/nodecli/read-file/index.html",
+    "revision": "9874added9edbaa7be26cf1916b4cdb2"
+  },
+  {
+    "url": "use-case/nodecli/refactor-and-unittest/index.html",
+    "revision": "8c14e34287d5c0f90f9d7fe916806ba8"
+  },
+  {
+    "url": "use-case/nodecli/refactor-and-unittest/src/test/fixtures/expected.html",
+    "revision": "98b8e1ceac14ee8d5bd405fe8fc1b2bc"
+  },
+  {
+    "url": "use-case/setup-local-env/index.html",
+    "revision": "c33b46991bd7cfd7b5f94faccb7bb0c9"
+  },
+  {
+    "url": "use-case/todoapp/app-structure/img/todo-html.png",
+    "revision": "a7e248a9554bc7f79bc635b4c373ed9c"
+  },
+  {
+    "url": "use-case/todoapp/app-structure/index.html",
+    "revision": "eee4c9c9fb3e2f27e10637516f59e04f"
+  },
+  {
+    "url": "use-case/todoapp/app-structure/todo-html/index.html",
+    "revision": "7982d73005aace941cf4c11764d8d601"
+  },
+  {
+    "url": "use-case/todoapp/entrypoint/first-entry/index.html",
+    "revision": "be5eee3981ec18c49a62181f3fa78987"
+  },
+  {
+    "url": "use-case/todoapp/entrypoint/img/first-entry.png",
+    "revision": "07fda3c2b654ee7b03f3ded0a58c1fff"
+  },
+  {
+    "url": "use-case/todoapp/entrypoint/index.html",
+    "revision": "f13b99c5963b49742cb385391b5714c4"
+  },
+  {
+    "url": "use-case/todoapp/entrypoint/module-entry/index.html",
+    "revision": "be5eee3981ec18c49a62181f3fa78987"
+  },
+  {
+    "url": "use-case/todoapp/entrypoint/module-scope/index.html",
+    "revision": "363ad66f11327d4fbd4584c65dbcde65"
+  },
+  {
+    "url": "use-case/todoapp/event-model/event-emitter/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  },
+  {
+    "url": "use-case/todoapp/event-model/index.html",
+    "revision": "46abaaf1c8465726a149a94b47fcc6ea"
+  },
+  {
+    "url": "use-case/todoapp/final/create-view/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  },
+  {
+    "url": "use-case/todoapp/final/final/index.html",
+    "revision": "9ffaa1daeb5dc9ad3021970002a5aa53"
+  },
+  {
+    "url": "use-case/todoapp/final/index.html",
+    "revision": "d051c1cb78ccb0fa4ce8dbe2f3f29a11"
+  },
+  {
+    "url": "use-case/todoapp/final/more/index.html",
+    "revision": "9ffaa1daeb5dc9ad3021970002a5aa53"
+  },
+  {
+    "url": "use-case/todoapp/form-event/add-todo-item/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  },
+  {
+    "url": "use-case/todoapp/form-event/img/add-todo-item.png",
+    "revision": "044faab6418bb0d5c4a50d43be55a0ac"
+  },
+  {
+    "url": "use-case/todoapp/form-event/img/form-event.png",
+    "revision": "a193d8abc60dcc1f78c117af7a23a946"
+  },
+  {
+    "url": "use-case/todoapp/form-event/index.html",
+    "revision": "c46fa85f2307158d1109556ae276f9df"
+  },
+  {
+    "url": "use-case/todoapp/form-event/prevent-event/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  },
+  {
+    "url": "use-case/todoapp/index.html",
+    "revision": "25512cdb83204d01f119bdba2dccd155"
+  },
+  {
+    "url": "use-case/todoapp/update-delete/add-checkbox/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  },
+  {
+    "url": "use-case/todoapp/update-delete/delete-feature/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  },
+  {
+    "url": "use-case/todoapp/update-delete/img/input-checkbox.png",
+    "revision": "44920eba3f5737a49e9cc4c0893c44dd"
+  },
+  {
+    "url": "use-case/todoapp/update-delete/index.html",
+    "revision": "64a7b27c4f2b023315a79f20f4bdbde7"
+  },
+  {
+    "url": "use-case/todoapp/update-delete/input-checkbox/index.html",
+    "revision": "9528b6b1e9c5c6c029843537ea4121f5"
+  },
+  {
+    "url": "use-case/todoapp/update-delete/update-feature/index.html",
+    "revision": "4b6e0d6f957ffad0659f2076151c97ca"
+  }
+]);
